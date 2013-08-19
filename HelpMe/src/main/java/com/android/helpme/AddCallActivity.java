9434544135dc15cc41f5ac0c38b51cf0ac29ff45
richiejk <richiejk@gmail.com>
2013-08-19 18:00:53 +0530

    Update AddCallActivity.java

--- HelpMe/src/main/java/com/android/helpme/AddCallActivity.java
+++ HelpMe/src/main/java/com/android/helpme/AddCallActivity.java
@@ -199,3 +199,50 @@ public class HelpMeDBHandler {
 
 
 */
+/*
+package com.android.helpme.models;
+
+/**
+ * Created by richie on 8/19/13.
+ */
+public class ContactModel {
+
+    public ContactModel(String phone, String name, int priority) {
+        this.phone = phone;
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        if(name==null){
+            return "unnamed";
+        }else{
+            return name;
+        }
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    String phone;
+    String name;
+    int priority;
+}
+*/
